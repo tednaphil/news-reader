2b2745d2487137063f44abca47a1f17b499d4882
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-04-30 13:05:56 -0500

    Add mock data file

---
+++ src/mock-data.js
@@ -0,0 +1,269 @@
+const data = {
+    "status": "ok",
+    "totalResults": 20,
+    "articles": [
+        {
+        "source": {
+            "id": null,
+            "name": "YouTube"
+        },
+        "author": null,
+        "title": "Home Office Loses Contact With Thousands Of Migrants Set To Rwanda | “I Want To Laugh!” - TalkTV",
+        "description": null,
+        "url": "https://www.youtube.com/watch?v=xrtFAehqdwc",
+        "urlToImage": null,
+        "publishedAt": "2024-04-30T15:15:08Z",
+        "content": "Your browser isnt supported anymore. Update it to get the best YouTube experience and our latest features. Learn more\r\nRemind me later"
+        },
+        {
+        "source": {
+            "id": null,
+            "name": "Yahoo Entertainment"
+        },
+        "author": "Josh Schafer",
+        "title": "US consumer confidence hits lowest level since July 2022 - Yahoo Finance",
+        "description": "Economic optimism from Americans fell sharply in April as inflation sticks around and the outlook on the job market sours.",
+        "url": "https://finance.yahoo.com/news/us-consumer-confidence-hits-lowest-level-since-july-2022-151304354.html",
+        "urlToImage": "https://s.yimg.com/ny/api/res/1.2/cIGMaesQGf9itNpFOxh4Ig--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://s.yimg.com/os/creatr-uploaded-images/2023-07/6444ee40-2022-11ee-ae76-31ab84342107",
+        "publishedAt": "2024-04-30T15:13:04Z",
+        "content": "Consumer confidence fell sharply in April as inflation worries and a downbeat outlook on the job market pushed optimism to its lowest level since 2022.\r\nThe Conference Board's consumer confidence ind… [+3721 chars]"
+        },
+        {
+        "source": {
+            "id": "associated-press",
+            "name": "Associated Press"
+        },
+        "author": "CEDAR ATTANASIO, JAKE OFFENHARTZ, JONATHAN MATTISE",
+        "title": "College anti-war protests: Columbia limits campus access to students, essential employees - The Associated Press",
+        "description": "Standoffs between pro-Palestinian protesters and U.S. college administrators appear to be coming to a head. Columbia University in New York limited campus access after protesters took over a building early Tuesday. Police at Yale University in Connecticut mov…",
+        "url": "https://apnews.com/article/israel-palestinian-campus-student-protests-war-8b0d3a0cedb17f5e892c6ca43bbdf628",
+        "urlToImage": "https://dims.apnews.com/dims4/default/9eb7053/2147483647/strip/true/crop/3000x1688+0+156/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F7a%2Fac%2F7df038263064fa44aeae4f1a9db0%2F553414cd610540f897f8eaada93e494e",
+        "publishedAt": "2024-04-30T14:59:00Z",
+        "content": "NEW YORK (AP) Dozens of protesters took over a building at Columbia University in New York early Tuesday, barricading the entrances and unfurling a Palestinian flag out of a window in the latest esca… [+8021 chars]"
+        },
+        {
+        "source": {
+            "id": null,
+            "name": "The Cut"
+        },
+        "author": "Tariro Mzezewa",
+        "title": "Barbra Streisand Asked Melissa McCarthy About Ozempic - The Cut",
+        "description": "Melissa McCarthy’s recent Instagram post prompted Barbra Streisand to ask if she’s taking Ozempic, sparking backlash from people accusing Streisand of being rude and disrespectful.",
+        "url": "http://www.thecut.com/article/barbra-streisand-melissa-mccarthy-ozempic.html",
+        "urlToImage": "https://pyxis.nymag.com/v1/imgs/71d/be7/125f13067e5cdcf463ce19029f2ad04ba0-GettyImages-585471072.1x.rsocial.w1200.jpg",
+        "publishedAt": "2024-04-30T14:50:04Z",
+        "content": "More than two years into the Ozempic craze, youd think everyone would know better than to ask people if theyre taking the medication for weight loss. But apparently not everyone got the memo. On Mond… [+1236 chars]"
+        },
+        {
+        "source": {
+            "id": null,
+            "name": "9to5google.com"
+        },
+        "author": "Ben Schoon",
+        "title": "Latest Pixel 8a leak reveals bigger battery and $499 starting price - 9to5Google",
+        "description": "A new leak of the Google Pixel 8a reveals the price won't be changed after all, as well as spilling the details on the battery size.",
+        "url": "http://9to5google.com/2024/04/30/google-pixel-8a-price-battery-leak/",
+        "urlToImage": "https://i0.wp.com/9to5google.com/wp-content/uploads/sites/4/2024/04/pixel-8a-leak-7.jpeg?resize=1200%2C628&quality=82&strip=all&ssl=1",
+        "publishedAt": "2024-04-30T14:41:00Z",
+        "content": "Another fresh leak of the Pixel 8a has revealed more images of the device, as well as the pricing in the US which, contrary to past rumors, may not be getting a price hike after all.\r\nThrough the pas… [+1199 chars]"
+        },
+        {
+        "source": {
+            "id": "the-wall-street-journal",
+            "name": "The Wall Street Journal"
+        },
+        "author": "The Wall Street Journal",
+        "title": "Judge Fines Trump for Contempt in Hush-Money Case, Threatens Jail Time - The Wall Street Journal",
+        "description": null,
+        "url": "https://www.wsj.com/us-news/judge-finds-trump-violated-gag-order-in-hush-money-case-4d20848b",
+        "urlToImage": null,
+        "publishedAt": "2024-04-30T14:30:00Z",
+        "content": null
+        },
+        {
+        "source": {
+            "id": null,
+            "name": "Yahoo Entertainment"
+        },
+        "author": "Will Dunham",
+        "title": "T. rex is at the center of a debate over dinosaur intelligence - Yahoo! Voices",
+        "description": "Surmising even the physical appearance of a dinosaur - or any extinct animal - based on its fossils is a tricky proposition, with so many uncertainties...",
+        "url": "https://www.yahoo.com/news/t-rex-center-debate-over-142828509.html",
+        "urlToImage": "https://media.zenfs.com/en/reuters.com/0e5a6ca7fbe755e6a8395d4bba577272",
+        "publishedAt": "2024-04-30T14:28:28Z",
+        "content": "By Will Dunham\r\nWASHINGTON (Reuters) - Surmising even the physical appearance of a dinosaur - or any extinct animal - based on its fossils is a tricky proposition, with so many uncertainties involved… [+4718 chars]"
+        },
+        {
+        "source": {
+            "id": "associated-press",
+            "name": "Associated Press"
+        },
+        "author": "MIKE CORDER",
+        "title": "ICJ will rule on Nicaragua's ask for halt on German weapons sales to Israel - The Associated Press",
+        "description": "The top United Nations court has rejected a request by Nicaragua to order Germany to halt military and other aid to Israel and renew funding to the U.N. aid agency in Gaza. The International Court of Justice said Tuesday that legal conditions for making such …",
+        "url": "https://apnews.com/article/gaza-icj-nicaragua-germany-israel-9c4601a3749fb51ae77ca43cadde4c1a",
+        "urlToImage": "https://dims.apnews.com/dims4/default/37ccc0e/2147483647/strip/true/crop/5669x3189+0+296/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F23%2Fee%2Fb9f5ee944226d8b46f3028914cf6%2Ff76f186842e04757b82cbb8988cce677",
+        "publishedAt": "2024-04-30T14:24:00Z",
+        "content": "THE HAGUE, Netherlands (AP) The top U.N. court rejected on Tuesday a request by Nicaragua to order Germany to halt military and other aid to Israel and renew funding to the U.N. aid agency in Gaza.\r\n… [+4997 chars]"
+        },
+        {
+        "source": {
+            "id": "the-washington-post",
+            "name": "The Washington Post"
+        },
+        "author": "Thomas Floyd",
+        "title": "Tony Award nominations 2024: Updating list of nominees and analysis - The Washington Post",
+        "description": "“Hell’s Kitchen,” “Illinoise,” “The Outsiders,” “Suffs” and “Water for Elephants” will contend for best musical at the Tony Awards, while Rachel McAdams, Jeremy Strong and Jessica Lange are also up for prizes.",
+        "url": "https://www.washingtonpost.com/entertainment/theater/2024/04/30/tony-award-nominations-2024-list/",
+        "urlToImage": "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/WLUGA2XNIB4S26OYNXCQPKUYHE.jpg&w=1440",
+        "publishedAt": "2024-04-30T14:07:20Z",
+        "content": "Hells Kitchen, Illinoise, The Outsiders, Suffs and Water for Elephants will contend for best musical at the Tony Awards and stars including Rachel McAdams, Jeremy Strong and Jessica Lange are also up… [+280 chars]"
+        },
+        {
+        "source": {
+            "id": null,
+            "name": "[Removed]"
+        },
+        "author": null,
+        "title": "[Removed]",
+        "description": "[Removed]",
+        "url": "https://removed.com",
+        "urlToImage": null,
+        "publishedAt": "1970-01-01T00:00:00Z",
+        "content": "[Removed]"
+        },
+        {
+        "source": {
+            "id": null,
+            "name": "MacRumors"
+        },
+        "author": "Eric Slivka",
+        "title": "Beats Solo 4 Headphones Debut With Improved Acoustics, Longer Battery Life, and More - MacRumors",
+        "description": "Apple's Beats brand today is announcing an update to its most popular line of headphones with the new Beats Solo 4. The on-ear headphones have...",
+        "url": "https://www.macrumors.com/2024/04/30/beats-solo-4-launch/",
+        "urlToImage": "https://images.macrumors.com/t/NbLO0I7yqKpSw25N1x1_V49AcRo=/1600x/article-new/2024/04/beats-solo-4-colors.jpg",
+        "publishedAt": "2024-04-30T14:00:00Z",
+        "content": "Apple's Beats brand today is announcing an update to its most popular line of headphones with the new Beats Solo 4. The on-ear headphones have been re-engineered inside and out to deliver upgraded pe… [+3467 chars]"
+        },
+        {
+        "source": {
+            "id": "usa-today",
+            "name": "USA Today"
+        },
+        "author": "Christopher Cann, Trevor Hughes",
+        "title": "Charlotte shooting suspect named after 4 officers killed in N.C. - USA TODAY",
+        "description": "Police in Charlotte are investigating one of the deadliest recent assaults on law enforcement after eight officers were shot Monday afternoon.",
+        "url": "https://www.usatoday.com/story/news/nation/2024/04/30/charlotte-police-shooting-suspect-named-updates/73509550007/",
+        "urlToImage": "https://www.usatoday.com/gcdn/authoring/authoring-images/2024/04/30/USAT/73510190007-untitled.png?crop=1919,1079,x0,y0&width=1919&height=1079&format=pjpg&auto=webp",
+        "publishedAt": "2024-04-30T13:41:15Z",
+        "content": "CHARLOTTE, N.C. Four law enforcement officers were killed and four others were injured Monday when gunfire erupted at a house in Charlotte, North Carolina, where authorities first arrived to serve a … [+6240 chars]"
+        },
+        {
+        "source": {
+            "id": null,
+            "name": "Space.com"
+        },
+        "author": "Mike Wall",
+        "title": "SpaceX's 30th Dragon cargo mission departs the ISS, splashes down on Earth - Space.com",
+        "description": "Dragon splashed down in the wee hours of April 30.",
+        "url": "https://www.space.com/spacex-dragon-cargo-mission-crs-30-iss-departure",
+        "urlToImage": "https://cdn.mos.cms.futurecdn.net/HLUVp759r5n4cTmZkNuvBc-1200-80.jpg",
+        "publishedAt": "2024-04-30T13:32:51Z",
+        "content": "SpaceX's 30th robotic Dragon cargo ship is back home to Earth.\r\nDragon departed from the International Space Station (ISS) today (April 28) at 1:10 p.m. EDT (1710 GMT), while the two spacecraft were … [+2286 chars]"
+        },
+        {
+        "source": {
+            "id": null,
+            "name": "MarketWatch"
+        },
+        "author": "Andrew Keshner",
+        "title": "Stock futures drop after data shows employee costs rising quickly - MarketWatch",
+        "description": "Follow all the latest U.S. market action for Tuesday here.",
+        "url": "https://www.marketwatch.com/livecoverage/stock-market-today-dow-futures-flat-with-amazon-results-in-focus/card/stock-futures-drop-after-data-shows-employee-costs-rising-quickly-du3qQoOegoY3MM2GmZZX",
+        "urlToImage": "https://images.mktw.net/im-21474590/social",
+        "publishedAt": "2024-04-30T13:07:30Z",
+        "content": "Back to TopIntraday Data provided by FACTSET and subject to terms of use. Historical and current end-of-day data provided by FACTSET. All quotes are in local exchange time. Real-time last sale data f… [+136 chars]"
+        },
+        {
+        "source": {
+            "id": null,
+            "name": "Pff.com"
+        },
+        "author": "Trevor Sikkema",
+        "title": "2024 NFL Draft grades for all 16 NFC teams | NFL Draft - Pro Football Focus",
+        "description": "The 2024 NFL Draft is over, and the grades are in for all 16 NFC teams.",
+        "url": "https://www.pff.com/news/draft-grades-16-nfc-teams-nfl-2024",
+        "urlToImage": "https://media.pff.com/2024/04/2X3G11C-scaled.jpg?w=1200&h=675",
+        "publishedAt": "2024-04-30T13:00:04Z",
+        "content": "The 2024 NFL Draft is officially in the books. After a flurry of selections from Thursday to Saturday, 257 players were selected to join the NFL. With that, we give you our full draft recap, with an … [+43984 chars]"
+        },
+        {
+        "source": {
+            "id": "the-washington-post",
+            "name": "The Washington Post"
+        },
+        "author": "Rachel Siegel",
+        "title": "Fed interest rate cuts likely to be pushed back amid high inflation - The Washington Post",
+        "description": "Since the start of the year, central bankers’ best hopes to take pressure off the economy have gotten dimmer and dimmer.",
+        "url": "https://www.washingtonpost.com/business/2024/04/30/fed-interest-rate-cuts-inflation/",
+        "urlToImage": "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/YJC6KNUQ5VA3V5WW4AY6FCKYE4.JPG&w=1440",
+        "publishedAt": "2024-04-30T13:00:00Z",
+        "content": "It wasnt long ago that the Federal Reserve looked on track to cut interest rates this spring. But those optimistic days sure feel like theyve slipped away.\r\nSince the start of the year, central banke… [+470 chars]"
+        },
+        {
+        "source": {
+            "id": "reuters",
+            "name": "Reuters"
+        },
+        "author": "Reuters",
+        "title": "Israel will enter Rafah with or without Gaza hostage deal, Netanyahu says - Reuters",
+        "description": null,
+        "url": "https://www.reuters.com/world/middle-east/israel-will-enter-rafah-with-or-without-gaza-hostage-deal-netanyahu-says-2024-04-30/",
+        "urlToImage": null,
+        "publishedAt": "2024-04-30T12:59:00Z",
+        "content": null
+        },
+        {
+        "source": {
+            "id": "cnn",
+            "name": "CNN"
+        },
+        "author": "Louis Mian",
+        "title": "Scores missing as Kenya ravaged by mass flooding - CNN",
+        "description": "At least 91 people are missing after heavy flooding across the Kenya’s capital, Nairobi, the government said Tuesday, after weeks of heavy rain and flash floods have ravaged parts of the country.",
+        "url": "https://www.cnn.com/2024/04/30/africa/kenya-nairobi-flooding-missing-people-intl/index.html",
+        "urlToImage": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2150180860.jpg?c=16x9&q=w_800,c_fill",
+        "publishedAt": "2024-04-30T12:46:00Z",
+        "content": "At least 91 people are missing after heavy flooding across Kenyas capital, Nairobi, the government said Tuesday, after weeks of heavy rain and flash floods ravaged parts of the country.\r\nSome 76 peop… [+2516 chars]"
+        },
+        {
+        "source": {
+            "id": "abc-news",
+            "name": "ABC News"
+        },
+        "author": "Jon Haworth",
+        "title": "Boy, 13, dies after man with sword stabs multiple people near London tube station - ABC News",
+        "description": "\"This must have been a terrifying incident for those concerned,\" police said.",
+        "url": "https://abcnews.go.com/International/man-sword-arrested-after-multiple-people-stabbed-london/story?id=109777288",
+        "urlToImage": "https://i.abcnewsfe.com/a/b06a80aa-8d05-4b1a-a8a2-0cfc6fed567c/hainault-police_1714469076947_hpMain_16x9.jpg?w=1600",
+        "publishedAt": "2024-04-30T12:43:14Z",
+        "content": "LONDON -- A teenager is dead and a man has been arrested after driving a car into a home in east London before getting out and attacking members of the public with a sword, according to police.\r\nThe … [+1568 chars]"
+        },
+        {
+        "source": {
+            "id": "cnn",
+            "name": "CNN"
+        },
+        "author": "Brenda Goodman",
+        "title": "Spikes of flu virus in wastewater raise questions about spread of bird flu - CNN",
+        "description": "Spikes of influenza A virus seen in wastewater samples from 59 sewer systems across 18 different states this spring may point to the spread of the H5N1 avian influenza virus that is currently infecting dairy cattle, a new study suggests.",
+        "url": "https://www.cnn.com/2024/04/30/health/bird-flu-wastewater-cattle/index.html",
+        "urlToImage": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1498551049.jpg?c=16x9&q=w_800,c_fill",
+        "publishedAt": "2024-04-30T12:38:00Z",
+        "content": "Spikes of influenza A virus seen in wastewater samples from 59 sewer systems across 18 different states this spring may point to the spread of the H5N1 avian influenza virus that is currently infecti… [+6685 chars]"
+        }
+    ]
+    }
+
+    export default data
+    
