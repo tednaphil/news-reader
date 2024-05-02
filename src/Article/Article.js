2bbcf1522c23d2a5fdad0ce662b23bcf30cf2db5
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-05-01 20:57:14 -0500

    Add rendered elements for non-existent article

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -13,24 +13,39 @@ function Article({articles}) {
         return articles.find(article => article.title === title)
     }
 
-    return (
-        <>
-        <Link to='/'>Back Home</Link>
-        <div className='article-view'>
-            <aside className='summary-column'>
-                <img src={chosenArticle.urlToImage} alt={`${chosenArticle.description}`} />
-                <p className="date">{chosenArticle.date}</p>
-                <p className="source">source: {chosenArticle.source.name}</p>
-            </aside>
-            <main className='content-column'>
-                <h2>{chosenArticle.title}</h2>
-                {chosenArticle.author && <h3>Author: {chosenArticle.author}</h3>}
-                <p>{chosenArticle.content}</p>
-                <a href={chosenArticle.url}>See full article</a>
-            </main>
-        </div>
-        </>
-    )
+    if(chosenArticle) {
+        return(
+            <>
+            <Link to='/'>Back Home</Link>
+            <div className='article-view'>
+                <aside className='summary-column'>
+                    <img src={chosenArticle.urlToImage} alt={`${chosenArticle.description}`} />
+                    <p className="date">{chosenArticle.date}</p>
+                    <p className="source">source: {chosenArticle.source.name}</p>
+                </aside>
+                <main className='content-column'>
+                    <h2>{chosenArticle.title}</h2>
+                    {chosenArticle.author && <h3>Author: {chosenArticle.author}</h3>}
+                    <p>{chosenArticle.content}</p>
+                    <a href={chosenArticle.url}>See full article</a>
+                </main>
+            </div>
+            </>
+        )
+    }
+
+    if(!chosenArticle) {
+        return(
+            <>
+                <h2>Whoops! Article not found.</h2>
+                <Link to='/'>Back Home</Link>
+            </>
+        )
+    }
 }
 
-export default Article
+export default Article;
+
+Article.propTypes = {
+    articles: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string))
+}
